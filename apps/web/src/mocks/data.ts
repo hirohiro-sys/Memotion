ab8467d7180475fde313f24eba150885890081d1
hirohiro-sys <katohiro8843@icloud.com>
2026-09-15 17:16:11 +0900

    既存のMSWによるモック呼び出しを削除

--- apps/web/src/mocks/data.ts
+++
@@ -1,9 +0,0 @@
-import type { NotificationSettings } from "@repo/shared";
-
-export const DEFAULT_NOTIFICATIONS: NotificationSettings = {
-  techWeeklyEnabled: true,
-  techWeeklyDay: 0,
-  techWeeklyTime: "20:00",
-  pendingCount: 0,
-  disabledReason: null,
-};
