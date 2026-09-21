ac6ca485c4e2d91766a19740556d2c0280c70a1e
hirohiro-sys <katohiro8843@icloud.com>
2026-09-21 16:54:39 +0900

    タグ判定ロジックにtodoを追加

--- apps/api/src/lib/line/client.test.ts
+++ apps/api/src/lib/line/client.test.ts
@@ -22,7 +22,7 @@ describe("replyFailure", () => {
       replyFailure({
         accessToken: "token",
         replyToken: "reply-1",
-        text: "使えるタグは #tweet #tech #other です。",
+        text: "使えるタグは #tweet #tech #other #todo です。",
       }),
     ).resolves.toBe(true);
 
@@ -39,7 +39,7 @@ describe("replyFailure", () => {
           messages: [
             {
               type: "text",
-              text: "使えるタグは #tweet #tech #other です。",
+              text: "使えるタグは #tweet #tech #other #todo です。",
             },
           ],
         }),
